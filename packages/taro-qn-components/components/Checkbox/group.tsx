8d20a4d167a8be1c2064e78631107c798a701caa
lazyperson <564981089@qq.com>
2021-11-22 17:31:57 +0800

    CheckBoxGroup 子元素样式拓展

--- packages/taro-qn-components/components/Checkbox/group.tsx
+++ packages/taro-qn-components/components/Checkbox/group.tsx
@@ -8,20 +8,9 @@ import CCheckbox from './index';
 const classSelector = `${prefixCls}-checkbox`;
 
 export default function Group(props) {
-  const {
-    children,
-    defaultValue,
-    value,
-    options,
-    onChange,
-    disabled,
-    layout,
-    style,
-  } = props;
+  const { children, defaultValue, value, options, onChange, disabled, layout, style, itemStyle } = props;
 
-  const [currentValue, setCurrentValue] = useState(
-    value === undefined ? defaultValue : value,
-  );
+  const [currentValue, setCurrentValue] = useState(value === undefined ? defaultValue : value);
 
   useEffect(() => {
     setCurrentValue(value === undefined ? defaultValue : value);
@@ -60,13 +49,7 @@ export default function Group(props) {
     if (typeof options !== 'undefined') {
       return getOptions().map((option) => {
         return (
-          <CCheckbox
-            disabled={option.disabled}
-            checked={currentValue.indexOf(option.value) !== -1}
-            value={option.value}
-            key={option.value}
-            onChange={checkboxChange}
-          >
+          <CCheckbox disabled={option.disabled} checked={currentValue.indexOf(option.value) !== -1} value={option.value} key={option.value} style={itemStyle} onChange={checkboxChange}>
             {option.label}
           </CCheckbox>
         );
@@ -75,10 +58,7 @@ export default function Group(props) {
     return _children;
   }
 
-  const checkboxes = useMemo(
-    () => renderChild(children),
-    [options, currentValue, disabled, children],
-  );
+  const checkboxes = useMemo(() => renderChild(children), [options, currentValue, disabled, children]);
 
   const classes = cls(`${classSelector}-group`, {
     [`${classSelector}-vertical`]: layout === 'vertical',
